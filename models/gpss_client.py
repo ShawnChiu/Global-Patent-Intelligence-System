e301fba020f6a0a45351d37316a3c1748506e975
Paidaxin0226 <lennycheng0910@gmail.com>
2025-11-28 14:59:33 +0800

    更改抓取圖表的對象

--- models/gpss_client.py
+++ models/gpss_client.py
@@ -67,25 +67,41 @@ class GPSSClient:
 
         self.page2.locator("select[name='fld4']").select_option("YP_0")
         self.page2.locator("select[name='limit4']").select_option("4")
-        self.page2.locator("select[name='fld2']").select_option("IP3")
-        self.page2.locator("select[name='limit2']").select_option("30")
+        with self.page2.expect_download() as download_info:
+            buttons[1].click()
+            self.page2.wait_for_load_state()
+        download = download_info.value
+        file_name = f"diagram_{1}.html"
+        destination_path = os.path.join(os.getcwd() + "/.data", file_name)
+        download.save_as(destination_path)
 
-        for i in range(3):
-            with self.page2.expect_download() as download_info:
-                buttons[i].click()
-                self.page2.wait_for_load_state()
-            download = download_info.value
-            file_name = f"diagram_{i+1}.html"
-            destination_path = os.path.join(os.getcwd() + "/.data", file_name)
-            download.save_as(destination_path)
-        
+        self.page2.locator("select[name='limit2']").select_option("30")
+        self.page2.locator("select[name='fld2']").select_option("AX")
+        with self.page2.expect_download() as download_info:
+            buttons[2].click()
+        download = download_info.value
+        file_name = f"diagram_{2}.html"
+        destination_path = os.path.join(os.getcwd() + "/.data", file_name)
+        download.save_as(destination_path)
+
+        self.page2.locator("select[name='limit2']").select_option("30")
         self.page2.locator("select[name='fld2']").select_option("AY")
+        with self.page2.expect_download() as download_info:
+            buttons[2].click()
+        download = download_info.value
+        file_name = f"diagram_{3}.html"
+        destination_path = os.path.join(os.getcwd() + "/.data", file_name)
+        download.save_as(destination_path)
+
+        self.page2.locator("select[name='limit2']").select_option("30")
+        self.page2.locator("select[name='fld2']").select_option("IP3")
         with self.page2.expect_download() as download_info:
             buttons[2].click()
         download = download_info.value
         file_name = f"diagram_{4}.html"
         destination_path = os.path.join(os.getcwd() + "/.data", file_name)
         download.save_as(destination_path)
+
 
         self.page2.close()
 
